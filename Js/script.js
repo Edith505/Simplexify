ae3d8016237c7b0a43125f432adae0fcaa8d1076
Edith Rand <edithrand505@gmail.com>
2023-06-15 16:43:01 +0200

    javascript

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,47 @@
-let submit = document.querySelector('.submit')
+function validerFormulaire() {
+    var variableDecision = document.getElementById('variable-decision').value;
+    var variableContrainte = document.getElementById('variable-contrainte').value;
+
+    if (variableDecision === '') {
+        afficherErreur('Veuillez remplir le champ Variable de décision.', 'variable-decision');
+        return false;
+    }
+
+    if (variableContrainte === '') {
+        afficherErreur('Veuillez remplir le champ Contraintes.', 'variable-contrainte');
+        return false;
+    }
+    return true;
+}
+
+function validerFormulaire2() {
+    var inputsContraintes = document.querySelectorAll('.div-contraint input[type="text"]');
+    
+    for (var i = 0; i < inputsContraintes.length; i++) {
+        var valeur = inputsContraintes[i].value;
+        
+        if (valeur.trim() === '') {
+            afficherErreur('Veuillez remplir tous les champs de contraintes.', inputsContraintes[i]);
+            return false;
+        }
+        
+        if (!estNombre(valeur)) {
+            afficherErreur('Veuillez entrer des nombres valides pour les champs de contraintes.', inputsContraintes[i]);
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function estNombre(valeur) {
+    return !isNaN(parseFloat(valeur)) && isFinite(valeur);
+}
+
+function afficherErreur(message, champId) {
+    var champ = document.getElementById(champId);
+    var erreur = document.createElement('p');
+    erreur.className = 'erreur';
+    erreur.innerHTML = message;
+    champ.parentNode.insertBefore(erreur, champ.nextSibling);
+}
